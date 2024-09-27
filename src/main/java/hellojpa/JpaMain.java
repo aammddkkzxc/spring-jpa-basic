69f82c25574696307bf84cb56c5fc7ccce821b1b
aammddkkzxc <ideal0427@naver.com>
2024-09-27 21:37:32 +0900

    feat: 양방향 연관관계와 연관관계의 주인 2 - 주의점, 정리 - 양방향 매핑시 연관관계의 주인에 값을 입력해야 한다. 다만 객체 그래프 탐색이나, 입력 직후 1차캐시된 상태에서 조회를 해올 때 원하는 대로 동작하지 않을 수 있다.(아직 db에 반영이 안되었기 때문) - 따라서 양쪽 둘다 입력해 주는것이 좋은데, 이때 편의 메소드를 사용하는 것이 좋음. 편의 메소드 위치는 작업하다가 필요하다고 판단되는 쪽에 자유롭게 사용하는데 절대 양쪽에 둘다 만들지 말아야 한다. - 단방향 매핑을 기본으로하고, 꼭 필요한 경우 양방향 사용한다.   - 사실 단방향으로 모두 가능하지만 jpql사용 시 역추적 할 일이 많아 필요한 경우가 생길 수 있다. (아직 구체적으로 어느상황인지는 모르겠다.. 조회할일이 엄청 많은 상황인가) - 양방향 매핑의 경우 사용에 주의해야함 @ToString이나 json 생성 라이브러리  사용 시 무한루프에 빠지는 미친 현상이 발생할 수 있다.    - 매핑된 곳에 toString 롬복 사용 X, 컨트롤러에서 엔티티 를 절대 반환하지 않는다. => dto사용

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,11 @@
 package hellojpa;
 
 import hellojpa.jpabook.entity.Member;
+import hellojpa.jpabook.entity.Order;
+import hellojpa.jpabook.entity.OrderStatus;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 public class JpaMain {
 
@@ -14,6 +18,25 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setName("hi");
+            em.persist(member);
+
+            Order order = new Order();
+            order.setOrderStatus(OrderStatus.ORDER);
+            order.setMember(member);
+            em.persist(order);
+
+//            member.getOrderList().add(order);
+
+//            em.flush();
+//            em.clear();
+//            Member findMember = em.find(Member.class, member.getId());
+//            List<Order> orders = findMember.getOrderList();
+//            for (Order oo : orders) {
+//                System.out.println(oo);
+//            }
+
 
             tx.commit();
         } catch (Exception e) {
